2d7cf55e031088989849ed63aa60ba4c0006de47
Mitendra Anand <mitendra@gmail.com>
2020-01-01 03:45:06 +0530

    implemented search model, view and app controller.

--- 7-recipe-mvc-app/src/js/index.js
+++ 7-recipe-mvc-app/src/js/index.js
@@ -1,6 +1,48 @@
-// Global app controller
-import num from './test';
-const x  = 123;
-console.log(`I imported ${num} from another module! Variable x is: ${x}`);
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
+import { renderLoader, clearLoader } from './views/base';
+
+
+/**Global state of the app
+ * Search object
+ * Current Recipe object
+ * Shapping list object
+ * Liked recipes
+*/
+const state = {
+
+};
+
+const controlSearch = async () => {
+    // 1) Get the query from the view
+    const query = searchView.getInput();
+
+    if (query) {
+        // 2) new Search object and add to the state.
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results.
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+
+        // 4) Search for recipes.
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        clearLoader();
+        searchView.renderResults(state.search.result);
+
+    }
+}
+
+elements.sarchFrom.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+})
+
+
 
 
---
+++ 7-recipe-mvc-app/src/js/views/base.js
@@ -0,0 +1,30 @@
+export const elements = {
+    sarchFrom: document.querySelector('.search'),
+    searchInput: document.querySelector('.search__field'),
+    searchResultList: document.querySelector('.results__list'),
+    searchRes: document.querySelector('.results')
+
+};
+
+export const elementstrings = {
+    loader: 'loader'
+}
+
+export const renderLoader =  parent => {
+    const loader = `
+        <div class="${elementstrings.loader}">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg>
+        </div>
+    `;
+    parent.insertAdjacentHTML('afterbegin', loader);
+}
+
+export const clearLoader = () => {
+    const loader = document.querySelector(`.${elementstrings.loader}`);
+    if (loader) {
+        loader.parentElement.removeChild(loader);
+    }
+
+}
