72fc4b0f038ad90139954c378fef63de7a2a822f
Mitendra Anand <mitendra@gmail.com>
2020-01-02 03:31:33 +0530

    implemented recipe - model/view/controller and shopping list - model

--- 7-recipe-mvc-app/src/js/index.js
+++ 7-recipe-mvc-app/src/js/index.js
@@ -2,6 +2,9 @@ import Search from './models/Search';
 import * as searchView from './views/searchView';
 import { elements } from './views/base';
 import { renderLoader, clearLoader } from './views/base';
+import Recipe from './models/Recipe';
+import * as recipeView from './views/recipeView';
+import List from './models/List';
 
 
 /**Global state of the app
@@ -10,13 +13,14 @@ import { renderLoader, clearLoader } from './views/base';
  * Shapping list object
  * Liked recipes
 */
-const state = {
+const state = {};
 
-};
 
-const controlSearch = async () => {
+const controlSearch = async (query = '') => {
     // 1) Get the query from the view
-    const query = searchView.getInput();
+    if (query.length === 0) {
+        query = searchView.getInput();
+    }
 
     if (query) {
         // 2) new Search object and add to the state.
@@ -27,20 +31,28 @@ const controlSearch = async () => {
         searchView.clearResults();
         renderLoader(elements.searchRes);
 
+        try {
+            // 4) Search for recipes.
+            await state.search.getResults();
 
-        // 4) Search for recipes.
-        await state.search.getResults();
-
-        // 5) Render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result);
-
+            // 5) Render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);            
+        } catch (error) {
+            alert('something wrong with the search.')
+            clearLoader();
+        }
     }
 }
 
 elements.sarchFrom.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
+})
+
+window.addEventListener('load', e => {
+    e.preventDefault();
+    controlSearch('pizza');
 })
 
 elements.searchResPages.addEventListener('click', e => {
@@ -56,3 +68,70 @@ elements.searchResPages.addEventListener('click', e => {
 
 
 
+/**
+ * RECIPE CONTROLLER
+ */
+
+ const controlRecipe = async () => {
+    // Get ID from url
+    const id = window.location.hash.replace('#','');
+    //console.log(id);
+
+    if (id) {
+        // Prepare UI for changes.
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
+
+        // Highlight Selected search
+        if (state.search) searchView.highlightSelected(id);
+
+        // Create new recipe object.
+        state.recipe = new Recipe(id);
+
+        // TESTING: So that inspect->console has access to recipe.
+        window.r = state.recipe;
+
+        try {
+            // Get recipe data.
+            await state.recipe.getRecipe();
+            state.recipe.parseIngredients();
+
+            // Calculate servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+
+            // Render recipe
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
+
+        } catch (error) {
+            alert('error processsing recipe!')
+        }
+    }
+
+ };
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+// Handling recipe button clicks
+elements.recipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+        // Decrease button is clicked
+        if (state.recipe.servings > 1) {
+            state.recipe.updateServings('dec');
+            recipeView.updateServingsIngredients(state.recipe);
+        }
+    } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+        // Increase button is clicked
+        state.recipe.updateServings('inc');
+        recipeView.updateServingsIngredients(state.recipe);
+    } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+        // Add ingredients to shopping list
+        controlList();
+    } else if (e.target.matches('.recipe__love, .recipe__love *')) {
+        // Like controller
+        controlLike();
+    }
+});
+
+window.l = new List();
--- 7-recipe-mvc-app/src/js/views/searchView.js
+++ 7-recipe-mvc-app/src/js/views/searchView.js
@@ -12,6 +12,23 @@ export const clearResults = () => {
     elements.searchResPages.innerHTML = '';
 }
 
+export const highlightSelected = id => {
+    const resultsArr = Array.from(document.querySelectorAll('.results__link'));
+    resultsArr.forEach(el => {
+        el.classList.remove('results__link--active');
+    });
+    const results_link = document.querySelector(`.results__link[href*="${id}"]`);
+    if (results_link) results_link.classList.add('results__link--active');
+};
+
+/*
+// 'Pasta with tomato and spinach'
+acc: 0 / acc + cur.length = 5 / newTitle = ['Pasta']
+acc: 5 / acc + cur.length = 9 / newTitle = ['Pasta', 'with']
+acc: 9 / acc + cur.length = 15 / newTitle = ['Pasta', 'with', 'tomato']
+acc: 15 / acc + cur.length = 18 / newTitle = ['Pasta', 'with', 'tomato']
+acc: 18 / acc + cur.length = 24 / newTitle = ['Pasta', 'with', 'tomato']
+*/
 const limitRecipeTitle = (title, limit = 17) => {
     const newTitle = [];
     if (title.length > limit) {
